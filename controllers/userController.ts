4ceca531bcf700b4345861e1b93e4faa0bf91840
Satish Ghode <satishghode9075@gmail.com>
2024-09-01 21:26:50 +0530

    Solved actication user error, complated login user and logout Auth APIs

--- controllers/userController.ts
+++ controllers/userController.ts
@@ -7,6 +7,7 @@ import jwt, { Secret } from "jsonwebtoken";
 import ejs from "ejs";
 import path from "path";
 import sendMail from "../utils/sendMail";
+import { sendToken } from "../utils/jwt";
 
 interface IRegistrationBody {
   name: string;
@@ -93,25 +94,22 @@ export const activateUser = CatchAsyncError(
     try {
       const { activation_code, activation_token } = req.body as IActivateRequest;
       
-      console.log(process.env.ACTIVATION_SECRET);
-
-      const newUser: { user: IUser; activationCode: string } = jwt.verify(
+      const newUser:{user: IUser; activationCode:string} = jwt.verify(
         activation_token,
-        process.env.ACTIVATION_SECRE as string
-      ) as { user: IUser; activationCode: string };
+        process.env.ACTIVATION_SECRET as string,
+      ) as {user: IUser; activationCode:string};
       
       if (newUser.activationCode !== activation_code) {
-        return new ErrorHandler("Invalid Activation Code", 400);
+        return next(new ErrorHandler("Invalid Activation Code", 400));
       }
 
       const { name, email, password } = newUser.user;
-      
 
       // check the email is already exits or not
       const existUser = await userModel.findOne({ email });      
 
       if (existUser) {
-        return new ErrorHandler("Email already exist", 400);
+        return next(new ErrorHandler("Email already exist", 400));
       }
 
       const user = await userModel.create({
@@ -124,7 +122,59 @@ export const activateUser = CatchAsyncError(
         success: true,
       });
     } catch (error: any) {
-      return new ErrorHandler("error.message", 400);
+      return next(new ErrorHandler(error.message, 400));
     }
   }
 );
+
+// login user
+
+interface ILoginRequest {
+  email: string;
+  password: string;
+}
+
+export const loginUser = CatchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { email, password } = req.body as ILoginRequest;
+      
+      if (!email || !password) {
+        return next(new ErrorHandler("Please enter email and password", 400));
+      }
+
+      const user = await userModel.findOne({ email }).select("+password");
+
+      if (!user) {
+        return next(new ErrorHandler("Invalid user name or password", 400));
+      }
+
+      const isPasswordMatch = await user.comparePassword(password);
+
+      if (!isPasswordMatch) {
+        return next(new ErrorHandler("Invalid user name or password", 400));
+      }
+
+      sendToken(user, 200, res);
+
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 400));
+    }
+  }
+);
+
+export const logoutUser = CatchAsyncError(async(req:Request, res: Response, next:NextFunction) => {
+  try {
+    res.cookie("access_token","",{maxAge: 1});
+    res.cookie("refresh_token","",{maxAge:1})
+    res.status(200).json({
+      success: true,
+      message:"Logged out successfully"
+    })
+    
+  } catch (error:any) {
+    return next(new ErrorHandler(error.message,400));
+    
+  }
+
+})
--- models/user.model.ts
+++ models/user.model.ts
@@ -1,8 +1,9 @@
+require("dotenv").config();
 import mongoose, { Document, Model, Schema } from "mongoose";
 import bcrypt from "bcryptjs";
-import { promises } from "dns";
+import jwt from "jsonwebtoken";
 
-const emailRegexPattren: RegExp =/^[^\a@]+@[^\@]+\.[^\s@]+$/;
+const emailRegexPattren: RegExp = /^[^\s@]+@[^\@]+\.[^\s@]+$/;
 
 // user Interface
 export interface IUser extends Document {
@@ -17,6 +18,8 @@ export interface IUser extends Document {
   isVerified: boolean;
   courses: Array<{ courseId: string }>;
   comparePassword: (password: string) => Promise<boolean>;
+  SignAccessToken: () => string;
+  SignRefreshToken: () => string;
 }
 
 const userSchema: Schema<IUser> = new mongoose.Schema(
@@ -71,6 +74,16 @@ userSchema.pre<IUser>("save", async function (next) {
   this.password = await bcrypt.hash(this.password, 10);
   next();
 });
+
+// sign access token
+userSchema.methods.SignAccessToken = function () {
+  return jwt.sign({ id: this.id }, process.env.ACCESS_TOKEN || "");
+};
+
+// sign refreshToken
+userSchema.methods.SignRefreshToken = function () {
+  return jwt.sign({ id: this.id }, process.env.REFRESH_TOKEN || "");
+};
 
 // compare password
 userSchema.methods.comparePassword = async function (
