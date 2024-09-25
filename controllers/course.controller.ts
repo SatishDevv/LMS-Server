7a499fff34151acf19958dee14fe9c05f6b257ae
Satish Ghode <satishghode9075@gmail.com>
2024-09-25 23:17:16 +0530

    Update course API complated

--- controllers/course.controller.ts
+++ controllers/course.controller.ts
@@ -3,6 +3,7 @@ import { CatchAsyncError } from "../middleware/catchAsyncError";
 import ErrorHandler from "../utils/ErrorHandler";
 import cloudinary from "cloudinary";
 import { createCourse } from "../services/course.service";
+import CourseModel from "../models/course.model";
 
 export const uploadCourse = CatchAsyncError(
   async (req: Request, res: Response, next: NextFunction) => {
@@ -21,10 +22,49 @@ export const uploadCourse = CatchAsyncError(
           url: myCloud.secure_url,
         };
       }
-      // 
+      //
       createCourse(data, res, next);
     } catch (error: any) {
       return next(new ErrorHandler(error.message, 500));
+    }
+  }
+);
+
+// edit course.
+export const editCourse = CatchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const data = req.body; // take the data form request body
+      const thaumbnail = data.thaumbnail;
+      //
+      if (thaumbnail) {
+        await cloudinary.v2.uploader.destroy(thaumbnail.public_id); // if hava an thaumbnail then delete  it.
+        // and upload new thaumbnail.
+        const myCloud = await cloudinary.v2.uploader.upload(thaumbnail, {
+          folder: "Courses",
+        });
+        // set the thaumbnail publid id and url
+        data.thaumbnail = {
+          public_id: myCloud.public_id,
+          url: myCloud.secure_url,
+        };
+      }
+      const courseId = req.params.id; // take the course id to update form params.
+      // find the course By Id and update the data into the DB.
+      const course = await CourseModel.findByIdAndUpdate(
+        courseId,
+        { $set: data },
+        { new: true }
+      );
+
+      res.status(200).json({
+        success: true,
+        course,
+      });
+
+
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 400));
     }
   }
 );
--- routes/course.route.ts
+++ routes/course.route.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import { authorizeRoles, isAutheticated } from "../middleware/auth";
-import { uploadCourse } from "../controllers/course.controller";
+import { editCourse, uploadCourse } from "../controllers/course.controller";
 const courseRouter = express.Router();
 
 courseRouter.post(
@@ -8,6 +8,13 @@ courseRouter.post(
   isAutheticated,
   authorizeRoles("admin"),
   uploadCourse
+);
+
+courseRouter.put(
+  "/edit-course/:id",
+  isAutheticated,
+  authorizeRoles("admin"),
+  editCourse
 );
 
 export default courseRouter;
